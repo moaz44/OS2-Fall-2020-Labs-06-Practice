6ee25f5612d8eb0efbe1f6f3cebac9fe7b31e324
moaz44 <76185434+moaz44@users.noreply.github.com>
2020-12-19 20:06:42 +0200

    Update Multiplier.java

--- src/MatrixMultiplication/Multiplier.java
+++ src/MatrixMultiplication/Multiplier.java
@@ -1,14 +1,34 @@
 package MatrixMultiplication;
 
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.function.Consumer;
+//start
 public class Multiplier {
     
     public static void multiply(double[][] matrix1, double[][] matrix2, double[][] result){
-        throw new UnsupportedOperationException("No implementation for multiply method, write your own implementation");
+        List threads = new ArrayList<>();
+  int rows1 = matrix1.length;
+  for (int i = 0; i < rows1; i++) {
+   MultiplierTask task ;
+   task = new MultiplierTask(result, matrix1, matrix2, i) {};
+   Thread thread = new Thread(task);
+   thread.start();
+   threads.add(thread);
+   if (threads.size() % 10 == 0) {
+    waitForThreads(threads);
+   }
+  }
     };
     
     protected static void waitForThreads(List<Thread> threads) {
-        throw new UnsupportedOperationException("No implementation for waitForThreads write your own implementatio");
+        threads.forEach((Thread thread) -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+            }
+        });
+  threads.clear();
+ }
     }
-}
+
