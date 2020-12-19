8c623bd3f5aaa2ca1484fb30a46293a7f536dfc5
moaz44 <76185434+moaz44@users.noreply.github.com>
2020-12-19 20:03:15 +0200

    Update MultiplierTask.java

--- src/MatrixMultiplication/MultiplierTask.java
+++ src/MatrixMultiplication/MultiplierTask.java
@@ -9,19 +9,37 @@ package MatrixMultiplication;
  *
  * @author AbdallaEssam
  */
+//start
 public abstract class MultiplierTask implements Runnable {
 
     private final double[][] result;
     private final double[][] matrix1;
     private final double[][] matrix2;
+    private final int row;
 
     public MultiplierTask(
             double[][] result,
             double[][] matrix1,
-            double[][] matrix2
+            double[][] matrix2,
+            int row
     ) {
         this.result = result;
         this.matrix1 = matrix1;
         this.matrix2 = matrix2;
+        this.row = row;
     }
+    
+    @Override
+    public void run() {
+
+  for (int i = 0; i < matrix2[0].length; i++) {
+   result[row][i] = 0;
+   for (int j = 0; j < matrix1[row].length; j++) {
+    result[row][i] += matrix1[row][j] * matrix2[j][i];
+
+   }
+
+  }
+
+ }
 }
